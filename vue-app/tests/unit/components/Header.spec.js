486fbdd569fa9888195209d9c470bbcf7df2936a
ryo-ma <saka_ro@yahoo.co.jp>
2020-03-16 16:15:41 +0900

    Add vue test

---
+++ vue-app/tests/unit/components/Header.spec.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Header from '@/components/Header.vue'
+
+import { localVue, before, after } from '@/../tests/vueCommon.js'
+
+beforeAll(before(jest))
+afterAll(after())
+
+describe('Header.vue', () => {
+
+  it('renders a header component', () => {
+
+    const wrapper = mount(Header)
+    expect(wrapper.attributes()).toMatchObject({class: 'header container'})
+    expect(wrapper.find('div.header').find('h2').text()).toEqual('配送ロボットデモ')
+  })
+})
